9832ab618d433a4c197b1f2a65b19c0b6c64f6ad
cf611535 <cf611535@fh-muenster.de>
2024-07-25 15:19:53 +0200

    remove comments in smime_name.py and add extendet message output

--- pkilint/cabf/smime/smime_name.py
+++ pkilint/cabf/smime/smime_name.py
@@ -173,11 +173,11 @@ class SubscriberSubjectValidator(validation.Validator):
 
         if self._required_one_of_n_attributes and len(self._required_one_of_n_attributes.intersection(attributes)) == 0:
             oids = oid.format_oids(self._required_one_of_n_attributes)
-            print("### added info start ###")
-            print(oids)
-            print("### added info end ###")
+            oids_url = ''
+            for oid in oids:
+                oids_url += "https://oidref.com/" + oid
             findings.append(validation.ValidationFindingDescription(self.VALIDATION_MISSING_ATTRIBUTE,
-                                                                    f'Missing one of these required attributes: {oids}')
+                                                                    f'Missing one of these required attributes: {oids} more information under {oids_url}')
                             )
 
         findings.extend((
