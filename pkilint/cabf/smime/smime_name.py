ba2751ec0f2e5fbe33ca49b83acecfc04a62e40a
cf611535 <cf611535@fh-muenster.de>
2024-07-26 17:11:18 +0200

    fix json

--- pkilint/cabf/smime/smime_name.py
+++ pkilint/cabf/smime/smime_name.py
@@ -16,8 +16,7 @@ SHALL_NOT = pkix.Rfc2119Word.SHALL_NOT
 MAY = pkix.Rfc2119Word.MAY
 
 
-_OID_METADATA = '''
-
+_OID_METADATA = [
     {
         "OID": "2.5.4.0",
         "Name": "objectClass",
@@ -704,8 +703,8 @@ _OID_METADATA = '''
         "Sub Nodes Total": "0",
         "Description": "organizationIdentifier ATTRIBUTE ::= {\nWITH SYNTAX UnboundedDirectoryString\nEQUALITY MATCHING RULE caseIgnoreMatch\nSUBSTRINGS M…"
     }
+]
 
-'''
 
 
 _MV_ATTRIBUTES = {
