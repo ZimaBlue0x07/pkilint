a5bab29e366a6edbb762107c0cc3c2fb63215d4c
cf611535 <cf611535@fh-muenster.de>
2024-07-30 10:59:48 +0200

    change output

--- pkilint/cabf/smime/smime_name.py
+++ pkilint/cabf/smime/smime_name.py
@@ -870,8 +870,9 @@ class SubscriberSubjectValidator(validation.Validator):
 
         if self._required_one_of_n_attributes and len(self._required_one_of_n_attributes.intersection(attributes)) == 0:
             oids = oid.format_oids(self._required_one_of_n_attributes)
+            oids_str = ""
             for oid in oids:
-                oids_str = oid_metadata[str(oid)["Name"]]
+                oids_str += oid_metadata[str(oid)["Name"]]
             findings.append(validation.ValidationFindingDescription(self.VALIDATION_MISSING_ATTRIBUTE, oids_str))
 
         findings.extend((
