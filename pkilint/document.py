5d6875c0f19e2b12ac43f0197c06508de83d2c12
cf611535 <cf611535@fh-muenster.de>
2024-08-13 13:23:49 +0200

    remove exception

--- pkilint/document.py
+++ pkilint/document.py
@@ -309,7 +309,7 @@ def decode_substrate(source_document: Document, substrate: bytes,
     else:
         try:
             decoded, rest = decode(substrate, asn1Spec=pdu_instance) # something went wrong here
-        except (ValueError, PyAsn1Error, SubstrateDecodingFailedError) as e:
+        except (ValueError, PyAsn1Error) as e:
             raise SubstrateDecodingFailedError(source_document, pdu_instance, parent_node, str(e)) from e
 
         decoded_pdu_name = get_node_name_for_pdu(decoded)
